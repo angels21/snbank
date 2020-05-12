b5ad43d8c2b56c90004f2c3808f2cedaf28397ad
angels21 <solotinted@yahoo.com>
2020-05-12 22:06:19 +0100

    added 1 file

--- banksys.py
+++ banksys.py
@@ -1,4 +1,6 @@
 import datetime
+from random import randint
+import sys
 
 login = str(input("For Staff login type 'yes'. To Close app type 'no'. \n"))
 if login == "yes":
@@ -28,49 +30,67 @@ if login == "yes":
             file.write("\n")
             file.close()
             print("You have successfully logged in! and your session has been saved.", username)
-            print("Type '1' to create bank account, type '2' to check account details or type '3' to logout" )
-            option = input("Type your option:")
-            if option == "1":
-                print("Please enter minimum of four characters (A-Z) as Customer's fullname")
-                customerName = input()
-                while len(customerName) < 4:
-                    print("Entry does not meet requirement, re-enter a minimum of four characters (A-Z) as Customer's fullname")
+
+            def createloop():
+                print("Type '1' to create bank account, type '2' to check account details or type '3' to logout" )
+                option = input("Type your option:")
+                if option == "1":
+                    print("Please enter minimum of four characters (A-Z) as Customer's fullname")
                     customerName = input()
+                    while len(customerName) < 4:
+                        print("Entry does not meet requirement, re-enter a minimum of four characters (A-Z) as Customer's fullname")
+                        customerName = input()
 
-                print('Please enter minimum of three digits (A-Z) as Customer account balance')
-                accountBalance = input()
-                while len(accountBalance) < 3:
-                    print('Entry does not meet requirement, re-enter a minimum of nine digits (A-Z) as account balance')
+                    print('Please enter minimum of three digits (A-Z) as Customer account balance')
                     accountBalance = input()
+                    while len(accountBalance) < 3:
+                        print('Entry does not meet requirement, re-enter a minimum of nine digits (A-Z) as account balance')
+                        accountBalance = input()
 
-                print('Please enter minimum of three characters (A-Z) as account type')
-                accountType = input()
-                while len(accountType) < 3:
-                    print('Entry does not meet requirement, re-enter minimum of three characters (A-Z) as account type')
+                    print('Please enter minimum of three characters (A-Z) as account type')
                     accountType = input()
+                    while len(accountType) < 3:
+                        print('Entry does not meet requirement, re-enter minimum of three characters (A-Z) as account type')
+                        accountType = input()
 
-                print('Please enter minimum of six characters (A-Z) as email address')
-                email = input()
-                while len(email) < 6:
-                    print('Entry does not meet requirement, re-enter minimum of six characters (A-Z) as email address')
+                    print('Please enter minimum of six characters (A-Z) as email address')
                     email = input()
-                file = open("customer.txt", "a")
-                file.write(customerName)
-                file.write(",")
-                file.write(accountBalance)
-                file.write(",")
-                file.write(accountType)
-                file.write(",")
-                file.write(email)
-                file.write(",")
-                file.write(datetime.datetime.now().ctime())
-                file.write("\n")
-                file.close()
-                break
-            elif option == "2":
-                break
-            elif option == "3":
-                break
+                    while len(email) < 6:
+                        print('Entry does not meet requirement, re-enter minimum of six characters (A-Z) as email address')
+                        email = input()
+                    accountNumber = str(2000000000+randint(1000000, 9999999))
+                    file = open("customer.txt", "a")
+                    file.write(customerName)
+                    file.write(",")
+                    file.write(accountBalance)
+                    file.write(",")
+                    file.write(accountType)
+                    file.write(",")
+                    file.write(email)
+                    file.write(",")
+                    file.write(accountNumber)
+                    file.write(",")
+                    file.write(datetime.datetime.now().ctime())
+                    file.write("\n")
+                    file.close()
+                    print("The ten digit account number for the newly created customer is", accountNumber)
+                    create_again = str(input("To create another account, type 'yes'. To return to the login page type 'no'. \n"))
+                    if create_again == "yes":
+                        createloop()
+                elif option == "2":
+                    #print("type the customer account number to print out his details")
+                    #line: str = input("account number:")
+                    file = open('customer.txt', 'r')
+                    print
+                    file.read()
+
+                elif option == "3":
+                    sys.exit('You have been succesfully logged out!')
+                else:
+                    print("Incorrect entry, type '1' to create bank account, type '2' to check account details or type '3' to logout" )
+                    createloop()
+            createloop()
+
 
 #file.close()
 
